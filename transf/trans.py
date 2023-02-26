b159e30d898418bfa0b9d34116d6336f0b8d67ba
MRSlou <79364553+MRSlouzk@users.noreply.github.com>
2023-02-26 16:01:26 +0800

    添加图片

--- transf/trans.py
+++ transf/trans.py
@@ -2,9 +2,9 @@ def webp_t():
     ffmpeg_path = "D:/ffmpeg/ffmpeg.exe"
 
     import os
-    for file in os.listdir("./input"):
+    for file in os.listdir("../input"):
         name = file.split('.')[0]
-        os.system(f"{ffmpeg_path} -i \"./input/{file}\" \"./meme/{name}.webp\"")
+        os.system(f"{ffmpeg_path} -i \"../input/{file}\" \"../meme/{name}.webp\"")
     print("已完成")
 
 def qq2md():
